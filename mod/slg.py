a8daf85c413546bb5887c42f85172f5d9e921a21
Bart Thate <bthate67@gmail.com>
2021-08-12 13:38:58 +0200

    update slug

--- mod/slg.py
+++ mod/slg.py
@@ -1,6 +1,6 @@
 # This file is placed in the Public Domain
 
-txt = "http://genocide.rtfd.io otp.informationdesk@icc-cpi.int OTP-CR-117/19"
+txt = "http://genocide.rtfd.io OTP-CR-117/19 email: otp.informationdesk@icc-cpi.int !!"
 
 def slg(event):
     event.reply(txt)
