5e09c52d4a375a063f47839925f42db69b758203
Bart Thate <bthate67@gmail.com>
2021-11-13 15:31:36 +0100

    update setup

--- setup.py
+++ setup.py
@@ -7,6 +7,23 @@ def read():
     return open("README.rst", "r").read()
 
 
+def uploadlist(dir):
+    upl = []
+
+    for file in os.listdir(dir):
+        if not file or file.startswith('.'):
+            continue
+        d = dir + os.sep + file
+        if os.path.isdir(d):   
+            upl.extend(uploadlist(d))
+        else:
+            if file.endswith(".pyc") or file.startswith("__pycache"):
+                continue
+            upl.append(d)
+    return upl
+
+
+
 setup(
     name="botlib",
     version="142",
@@ -18,6 +35,13 @@ setup(
     license="Public Domain",
     packages=["bot"],
     zip_safe=True,
+    include_package_data=True,
+    data_files=[
+                ('share/doc/botd', uploadlist("docs")),
+                ('share/botd', uploadlist("files")),
+                ("share/botd/", ["files/botd.service",],)
+    ],
+    scripts=["bin/bot", "bin/botc", "bin/botctl", "bin/botd"],
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: Public Domain",
