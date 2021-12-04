9d82d572fbe514f40d16038b059e259085f49deb
Bart Thate <bthate67@gmail.com>
2021-12-04 17:57:40 +0100

    add bin/fresh .. remove docs

--- setup.py
+++ setup.py
@@ -1,29 +1,11 @@
 # This file is placed in the Public Domain.
 
 
-import os
-
-
 from setuptools import setup
 
 
 def read():
     return open("README.rst", "r").read()
-
-
-def uploadlist(dir):
-    upl = []
-    for file in os.listdir(dir):
-        if not file or file.startswith('.'):
-            continue
-        d = dir + os.sep + file
-        if os.path.isdir(d):   
-            upl.extend(uploadlist(d))
-        else:
-            if file.endswith(".pyc") or file.startswith("__pycache"):
-                continue
-            upl.append(d)
-    return upl
 
 
 setup(
