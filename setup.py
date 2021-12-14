a32ec7b8ad1f070226601219fcfd3da3428b2b87
Bart Thate <bthate67@gmail.com>
2021-12-14 06:48:27 +0100

    up version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read():
 
 setup(
     name="botlib",
-    version="146",
+    version="147",
     url="https://github.com/bthate/botlib",
     author="Bart Thate",
     author_email="bthate67@gmail.com",
