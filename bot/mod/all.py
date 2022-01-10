be7b6de62b4f97853c976f6e59efe0302ee9d4b2
Bart Thate <bthate67@gmail.com>
2022-01-10 19:38:50 +0100

    remove debug

--- bot/mod/all.py
+++ bot/mod/all.py
@@ -5,18 +5,12 @@ from bot.tbl import Tbl
 
 
 from bot.mod import bsc
-from bot.mod import dbg
 from bot.mod import irc
-from bot.mod import req
 from bot.mod import rss
 from bot.mod import udp
-from bot.mod import wsd
 
 
 Tbl.add(bsc)
-Tbl.add(dbg)
 Tbl.add(irc)
-Tbl.add(req)
 Tbl.add(rss)
 Tbl.add(udp)
-Tbl.add(wsd)
